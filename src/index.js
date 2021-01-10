550a5c68f8708c9cc10ce2d45467491bf46f30c6
error696a <hoangnguyen789a@gmail.com>
2021-01-10 17:01:12 +0700

    read from database

--- src/index.js
+++ src/index.js
@@ -28,11 +28,11 @@ app.use(morgan('combined'));
 // Template engine
 app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
