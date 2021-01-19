b5dc2d755ccf2eea56931b3f923b5869b44fcfa6
error696a <hoangnguyen789a@gmail.com>
2021-01-19 23:06:33 +0700

    add sort courses by name, level, date

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,8 +3,16 @@ const { multipleMongooseToObject } = require('../../util/mongoose');
 class MeController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
+        let courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+
         Promise.all([
-            Course.find({}),
+            courseQuery,
             Course.countDocumentsDeleted(),
             Course.countDocuments(),
         ])
---
+++ src/app/middlewares/SortMiddleware.js
@@ -0,0 +1,18 @@
+function Middleware(req, res, next) {
+    res.locals._sort = {
+        enabled: false,
+        type: 'default',
+    };
+
+    if (req.query.hasOwnProperty('_sort')) {
+        Object.assign(res.locals._sort, {
+            enabled: true,
+            column: req.query.column,
+            type: req.query.type,
+        });
+    }
+
+    next();
+}
+
+module.exports = Middleware;
